18951cab533c802ada0f382904c3180d72be80f3
Alejandro Pulido <alexpulido33@gmail.com>
2020-08-23 01:53:35 -0500

    js done

--- testimonials.js
+++ testimonials.js
@@ -1,12 +1,13 @@
 let slideIndex = 1;
+// TODO: use Default parameters instead
 showSlides(slideIndex);
 
 function plusSlides(n) {
+	// ++ OPERATOR
 	showSlides((slideIndex += n));
 }
 
 function showSlides(n) {
-	let i;
 	let slides = document.getElementsByClassName("my-slides");
 	if (n > slides.length) {
 		slideIndex = 1;
@@ -14,8 +15,10 @@ function showSlides(n) {
 	if (n < 1) {
 		slideIndex = slides.length;
 	}
-	for (let i = 0; i < slides.length; i++) {
-		slides[i].style.display = "none";
+	// OPERATOR REPLACED
+	for (const slider of slides) {
+		slider.style.display = "none";
 	}
+
 	slides[slideIndex - 1].style.display = "block";
 }
