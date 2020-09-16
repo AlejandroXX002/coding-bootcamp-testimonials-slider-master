f34cc91a85083f8d9e1b6aed203e5e003c6aa696
Alejandro Pulido <alexpulido33@gmail.com>
2020-09-16 15:24:55 -0500

    final-design

--- testimonials.js
+++ testimonials.js
@@ -1,9 +1,7 @@
 let slideIndex = 1;
-// TODO: use Default parameters instead
 showSlides(slideIndex);
 
 function plusSlides(n) {
-	// ++ OPERATOR
 	showSlides((slideIndex += n));
 }
 
@@ -15,10 +13,9 @@ function showSlides(n) {
 	if (n < 1) {
 		slideIndex = slides.length;
 	}
-	// OPERATOR REPLACED
 	for (const slider of slides) {
 		slider.style.display = "none";
 	}
 
-	slides[slideIndex - 1].style.display = "block";
+	slides[slideIndex - 1].style.display = "";
 }
